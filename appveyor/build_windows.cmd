f43d57f75014de1fb0c8684b15b744e2f6b5ceb1
T. Isaac Lightburn <isaac@hozed.net>
2018-04-19 01:06:53 -0500

    Update build_windows.cmd

--- appveyor/build_windows.cmd
+++ appveyor/build_windows.cmd
@@ -14,7 +14,7 @@ qmake CONFIG-=debug CONFIG+=release
 nmake
 
 echo Packaging...
-cd %project_dir%\build\windows\msvc\x86_64\release\
+cd %project_dir%\build\windows\msvc\%LONGARCH%\release\
 dir
 windeployqt OpenKJ\release\OpenKJ.exe
 echo Signing OpenKJ binary
@@ -29,27 +29,27 @@ copy "%project_dir%\README.md" "OpenKJ\README.md"
 copy "%project_dir%\LICENSE" "OpenKJ\LICENSE.txt"
 
 echo Copying files for installer...
-mkdir "%project_dir%\installer\windows\x86_64\packages\org.openkj.openkj\data\"
-robocopy OpenKJ\release\ "%project_dir%\installer\windows\x86_64\packages\org.openkj.openkj\data" /E /np
-del "%project_dir%\installer\windows\x86_64\packages\org.openkj.openkj\data\*.obj"
-del "%project_dir%\installer\windows\x86_64\packages\org.openkj.openkj\data\*.cpp"
-del "%project_dir%\installer\windows\x86_64\packages\org.openkj.openkj\data\*.h"
+mkdir "%project_dir%\installer\windows\%LONGARCH%\packages\org.openkj.openkj\data\"
+robocopy OpenKJ\release\ "%project_dir%\installer\windows\%LONGARCH%\packages\org.openkj.openkj\data" /E /np
+del "%project_dir%\installer\windows\%LONGARCH%\packages\org.openkj.openkj\data\*.obj"
+del "%project_dir%\installer\windows\%LONGARCH%\packages\org.openkj.openkj\data\*.cpp"
+del "%project_dir%\installer\windows\%LONGARCH\packages\org.openkj.openkj\data\*.h"
 
 
 echo Pulling gstreamer deps for installer...
-copy c:\gstreamer\1.0\x86_64\bin\*.dll "%project_dir%\installer\windows\x86_64\packages\org.openkj.openkj\data\"
-mkdir "%project_dir%\installer\windows\x86_64\packages\org.openkj.openkj\data\lib"
-mkdir "%project_dir%\installer\windows\x86_64\packages\org.openkj.openkj\data\lib\gstreamer-1.0"
-mkdir "%project_dir%\installer\windows\x86_64\packages\org.openkj.openkj\data\lib\gstreamer-1.0\validate"
-copy c:\gstreamer\1.0\x86_64\lib\gstreamer-1.0\*.dll "%project_dir%\installer\windows\x86_64\packages\org.openkj.openkj\data\lib\gstreamer-1.0\"
-copy c:\gstreamer\1.0\x86_64\lib\gstreamer-1.0\validate\*.dll "%project_dir%\installer\windows\x86_64\packages\org.openkj.openkj\data\lib\gstreamer-1.0\validate\"
+copy c:\gstreamer\1.0\%LONGARCH%\bin\*.dll "%project_dir%\installer\windows\%LONGARCH%\packages\org.openkj.openkj\data\"
+mkdir "%project_dir%\installer\windows\%LONGARCH%\packages\org.openkj.openkj\data\lib"
+mkdir "%project_dir%\installer\windows\%LONGARCH%\packages\org.openkj.openkj\data\lib\gstreamer-1.0"
+mkdir "%project_dir%\installer\windows\%LONGARCH%\packages\org.openkj.openkj\data\lib\gstreamer-1.0\validate"
+copy c:\gstreamer\1.0\%LONGARCH%\lib\gstreamer-1.0\*.dll "%project_dir%\installer\windows\%LONGARCH%\packages\org.openkj.openkj\data\lib\gstreamer-1.0\"
+copy c:\gstreamer\1.0\%LONGARCH%\lib\gstreamer-1.0\validate\*.dll "%project_dir%\installer\windows\%LONGARCH%\packages\org.openkj.openkj\data\lib\gstreamer-1.0\validate\"
 
 
 rem echo Packaging portable archive...
 rem 7z a -bd OpenKJ_%OKJVERSION%_windows_x86_64_portable.zip OpenKJ
 
 echo Creating installer...
-cd %project_dir%\installer\windows\x86_64\
+cd %project_dir%\installer\windows\%LONGARCH%\
 dir
-binarycreator.exe --offline-only -c config\config.xml -p packages OpenKJ-%OKJVERSION%-windows-x86_64-installer.exe
-signtool sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /f "%project_dir%\cscrt\cscrt.pfx" /p "%pfx_pass%" OpenKJ-%OKJVERSION%-windows-x86_64-installer.exe
+binarycreator.exe --offline-only -c config\config.xml -p packages OpenKJ-%OKJVERSION%-windows-%LONGARCH%-installer.exe
+signtool sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /f "%project_dir%\cscrt\cscrt.pfx" /p "%pfx_pass%" OpenKJ-%OKJVERSION%-windows-%LONGARCH%-installer.exe
