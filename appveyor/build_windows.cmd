33ada7ea34a1d8540ad9c66b3768fd270a0abb44
T. Isaac Lightburn <isaac@hozed.net>
2018-04-18 21:34:31 -0500

    Update build_windows.cmd

--- appveyor/build_windows.cmd
+++ appveyor/build_windows.cmd
@@ -6,6 +6,9 @@ echo Set up environment...
 set PATH=%QT%\bin\;C:\Qt\Tools\QtCreator\bin\;C:\Qt\QtIFW2.0.1\bin\;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %PLATFORM%
 
+mkdir "%project_dir%\cscrt
+7z e "%project_dir%\installer\cscrt.7z" -p"%cscrt_pass%" -o"%project_dir%\cscrt"
+
 echo Building OpenKJ...
 qmake CONFIG-=debug CONFIG+=release
 nmake
