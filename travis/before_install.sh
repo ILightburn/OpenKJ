8a10aee9921428d551d00cae8f8c442aa6e4b7a9
T. Isaac Lightburn <isaac@hozed.net>
2018-04-27 10:25:00 -0500

    Update before_install.sh

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -8,3 +8,6 @@ export QT_INSTALLER_FILENAME=${QT_INSTALLER_ROOT}.dmg
 export QT_PATH=$HOME/qt
 export QT_MACOS=$QT_PATH/$QT_SHORT_VERSION/clang_64
 export PATH=$PATH:$QT_MACOS/bin
+
+chmod 755 ./travis/install.sh
+chmod 755 ./travis/build.sh
