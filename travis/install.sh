f40fcb357a668ed532bade95cc3e9c5015c0dde0
T. Isaac Lightburn <isaac@hozed.net>
2018-04-27 10:30:16 -0500

    Update install.sh

--- travis/install.sh
+++ travis/install.sh
@@ -20,7 +20,7 @@ echo "QT_LONG_VERSION QT_LONG_VERSION"
 if [[ "$QMAKE_VERSION" != "${QT_LONG_VERSION}" ]]; then
   rm -rf $QT_PATH
   echo "Downloading Qt"
-  wget -c --no-check-certificate -nv https://download.qt.io/archive/qt/${QT_SHORT_VERSION}/${QT_LONG_VERSION}/${QT_INSTALLER_FILENAME}
+  wget -c --no-check-certificate -nv https://download.qt.io/official_releases/qt/${QT_SHORT_VERSION}/${QT_LONG_VERSION}/${QT_INSTALLER_FILENAME}
   hdiutil mount ${QT_INSTALLER_FILENAME}
   cp -rf /Volumes/${QT_INSTALLER_ROOT}/${QT_INSTALLER_ROOT}.app $HOME/${QT_INSTALLER_ROOT}.app
   QT_INSTALLER_EXE=$HOME/${QT_INSTALLER_ROOT}.app/Contents/MacOS/${QT_INSTALLER_ROOT}
