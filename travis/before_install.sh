d943956b6ab3735ef22d7587c41870b796297a66
T. Isaac Lightburn <isaac@hozed.net>
2018-04-27 10:35:47 -0500

    Update before_install.sh

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -2,7 +2,7 @@
 
 export QT_SHORT_VERSION=5.10
 export QT_LONG_VERSION=5.10.1
-export QT_INSTALLER_ROOT=qt-opensource-mac-x64-clang-${QT_LONG_VERSION}
+export QT_INSTALLER_ROOT=qt-opensource-mac-x64-${QT_LONG_VERSION}
 export QT_INSTALLER_FILENAME=${QT_INSTALLER_ROOT}.dmg
 
 export QT_PATH=$HOME/qt
