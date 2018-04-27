0aeba3164d278a8e47a1d086a32da796ff1146ed
T. Isaac Lightburn <isaac@hozed.net>
2018-04-27 11:25:30 -0500

    Update install.sh

--- travis/install.sh
+++ travis/install.sh
@@ -8,9 +8,9 @@ fi
 #install gstreamer
 echo "gstreamer install"
 echo "Downloading gstreamer runtime package"
-/usr/local/aria2/bin/aria2c -x 10 https://gstreamer.freedesktop.org/data/pkg/osx/1.11.2/gstreamer-1.0-1.11.2-x86_64.pkg;
+wget -c --no-check-certificate -nv https://gstreamer.freedesktop.org/data/pkg/osx/1.11.2/gstreamer-1.0-1.11.2-x86_64.pkg
 echo "Downloading gstreamer devel package"
-/usr/local/aria2/bin/aria2c -x 10 https://gstreamer.freedesktop.org/data/pkg/osx/1.11.2/gstreamer-1.0-devel-1.11.2-x86_64.pkg;
+wget -c --no-check-certificate -nv https://gstreamer.freedesktop.org/data/pkg/osx/1.11.2/gstreamer-1.0-devel-1.11.2-x86_64.pkg
 echo "Installing gstreamer runtime package"
 sudo installer -package gstreamer-1.0-1.11.2-x86_64.pkg -target /;
 echo "Installing gstreamer devel package"
