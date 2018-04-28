acb6327397bbcb61719bd2594cdc8990633e6c53
T. Isaac Lightburn <isaac@hozed.net>
2018-04-28 17:06:40 -0500

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -7,11 +7,28 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   exit 0
 fi
 
-ls /
-ls /Library/Frameworks
-
+BundlePath=$PWD/OpenKJ.app
 
 $HOME/Qt/5.10.0/clang_64/bin/qmake
 
-make -j2
+make -j4
+
+$HOME/Qt/5.10.0/clang_64/bin/macdeployqt OpenKJ.app
+echo "Removing unneeded and non-appstore compliant plugins"
+rm -f OpenKJ.app/Contents/PlugIns/sqldrivers/libqsqlmysql.dylib
+rm -f OpenKJ.app/Contents/PlugIns/sqldrivers/libqsqlodbc.dylib
+rm -f OpenKJ.app/Contents/PlugIns/sqldrivers/libqsqlpsql.dylib
+echo "Copying GStreamer framework to package dir"
+cp -pR /Library/Frameworks/GStreamer.framework.deploy ${BundlePath}/Contents/Frameworks/GStreamer.framework
+echo "Fixing directory structure in the GStreamer framework"
+rm -f ${BundlePath}/Contents/Frameworks/GStreamer.framework/Versions/Current
+cd ${BundlePath}/Contents/Frameworks/GStreamer.framework/Versions
+ln -s 1.0 Current
+cd -
+echo "Modifying linker path info for GStreamer library to app bundle pathing"
+osxrelocator ${BundlePath}/Contents/Frameworks/GStreamer.framework/Versions/Current/lib /Library/Frameworks/GStreamer.framework/ /Applications/OpenKJ.app/Contents/Frameworks/GStreamer.framework/ -r &>/dev/null
+osxrelocator ${BundlePath}/Contents/Frameworks/GStreamer.framework/Versions/Current/libexec /Library/Frameworks/GStreamer.framework/ /Applications/OpenKJ.app/Contents/Frameworks/GStreamer.framework -r &>/dev/null
+osxrelocator ${BundlePath}/Contents/Frameworks/GStreamer.framework/Versions/Current/bin /Library/Frameworks/GStreamer.framework/ /Applications/OpenKJ.app/Contents/Frameworks/GStreamer.framework/ -r &>/dev/null
+osxrelocator ${BundlePath}/Contents/MacOS /Library/Frameworks/GStreamer.framework/ /Applications/OpenKJ.app/Contents/Frameworks/GStreamer.framework/ -r &>/dev/null
+
 
