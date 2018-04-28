68c38ed27fce1c936f9fd10d9d298a53c97e41ef
T. Isaac Lightburn <isaac@hozed.net>
2018-04-28 16:55:07 -0500

    Update install.sh

--- travis/install.sh
+++ travis/install.sh
@@ -5,8 +5,6 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   exit 0
 fi
 
-
-
 #install gstreamer#install Qt
 echo "Downloading Qt"
 wget -c --no-check-certificate -nv -Oqt.tar.bz2 https://cloud.hm.hozed.net/index.php/s/3lyFyolHbBdMx8o/download
@@ -23,8 +21,11 @@ echo "Downloading gstreamer devel package"
 wget -c --no-check-certificate -nv https://gstreamer.freedesktop.org/data/pkg/osx/1.11.2/gstreamer-1.0-devel-1.11.2-x86_64.pkg
 echo "Installing gstreamer runtime package"
 sudo installer -package gstreamer-1.0-1.11.2-x86_64.pkg -target /;
+echo "Making a deployment copy of the runtime"
+cp -R /Library/Frameworks/GStreamer.framework /Library/Frameworks/GStreamer.framework.deploy
 echo "Installing gstreamer devel package"
 sudo installer -package gstreamer-1.0-devel-1.11.2-x86_64.pkg -target /;
+ln -s /Users/travis /Users/lightburnisaac
 echo "gstreamer install done"
 
 
