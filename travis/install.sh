c251baac67ddd0c493b7a704b67490989094f825
T. Isaac Lightburn <isaac@hozed.net>
2018-04-28 16:22:07 -0500

    Update install.sh

--- travis/install.sh
+++ travis/install.sh
@@ -9,7 +9,7 @@ fi
 
 #install gstreamer#install Qt
 echo "Downloading Qt"
-wget -c --no-check-certificate -nv -Oqt.tbz2 https://cloud.hm.hozed.net/index.php/s/3lyFyolHbBdMx8o
+wget -c --no-check-certificate -nv -Oqt.tar.bz2 https://cloud.hm.hozed.net/index.php/s/3lyFyolHbBdMx8o
 echo "Extracting Qt"
 tar -xf qt.tar.bz2
 echo "Moving Qt to proper location"
