289322ccf08a2f1f5ecece224baf2c265f643555
T. Isaac Lightburn <isaac@hozed.net>
2018-04-28 16:18:27 -0500

    Update install.sh

--- travis/install.sh
+++ travis/install.sh
@@ -5,7 +5,16 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   exit 0
 fi
 
-#install gstreamer
+
+
+#install gstreamer#install Qt
+echo "Downloading Qt"
+wget -c --no-check-certificate -nv -Oqt.tbz2 https://cloud.hm.hozed.net/index.php/s/3lyFyolHbBdMx8o
+echo "Extracting Qt"
+tar -xf qt.tbz2
+echo "Moving Qt to proper location"
+mv Qt $HOME/Qt
+
 echo "gstreamer install"
 echo "Downloading gstreamer runtime package"
 wget -c --no-check-certificate -nv https://gstreamer.freedesktop.org/data/pkg/osx/1.11.2/gstreamer-1.0-1.11.2-x86_64.pkg
@@ -17,11 +26,5 @@ echo "Installing gstreamer devel package"
 sudo installer -package gstreamer-1.0-devel-1.11.2-x86_64.pkg -target /;
 echo "gstreamer install done"
 
-#install Qt
-echo "Downloading Qt"
-wget -c --no-check-certificate -nv -Oqt.tbz2 https://cloud.hm.hozed.net/index.php/s/3lyFyolHbBdMx8o
-echo "Extracting Qt"
-tar -xjf qt.tbz2
-echo "Moving Qt to proper location"
-mv Qt $HOME/Qt
+
 
