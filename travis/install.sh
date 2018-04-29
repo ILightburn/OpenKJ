2d35525e845086d36049c1a2de42dc7fcdba3e4c
T. Isaac Lightburn <isaac@hozed.net>
2018-04-28 19:50:08 -0500

    Update install.sh

--- travis/install.sh
+++ travis/install.sh
@@ -11,28 +11,35 @@ pip2 install osxrelocator
 echo "Installing appdmg"
 npm install -g appdmg
 
-#install gstreamer#install Qt
-echo "Downloading Qt"
-wget -c --no-check-certificate -nv -Oqt.tar.bz2 https://cloud.hm.hozed.net/index.php/s/3lyFyolHbBdMx8o/download
-echo "Extracting Qt"
-bunzip2 qt.tar.bz2
-tar -xf qt.tar
-echo "Moving Qt to proper location"
-mv Qt $HOME/Qt
+if [ -d "Qt" ]; then
+  echo "Cached copy of Qt already exists, skipping install"
+else
+  #install gstreamer#install Qt
+  echo "Downloading Qt"
+  wget -c --no-check-certificate -nv -Oqt.tar.bz2 https://cloud.hm.hozed.net/index.php/s/3lyFyolHbBdMx8o/download
+  echo "Extracting Qt"
+  bunzip2 qt.tar.bz2
+  tar -xf qt.tar
+  echo "Moving Qt to proper location"
+  mv Qt $HOME/Qt
+fi
 
-echo "gstreamer install"
-echo "Downloading gstreamer runtime package"
-wget -c --no-check-certificate -nv -Ogstreamer.pkg https://cloud.hm.hozed.net/index.php/s/MmEJPabg9FPnuCL/download
-echo "Downloading gstreamer devel package"
-wget -c --no-check-certificate -nv -Ogstreamer-dev.pkg https://cloud.hm.hozed.net/index.php/s/r41LnQOTjf1WG17/download
-echo "Installing gstreamer runtime package"
-sudo installer -package gstreamer.pkg -target /;
-echo "Making a deployment copy of the runtime"
-sudo cp -R /Library/Frameworks/GStreamer.framework /Library/Frameworks/GStreamer.framework.deploy
-echo "Installing gstreamer devel package"
-sudo installer -package gstreamer-dev.pkg -target /;
-sudo ln -s /Users/travis /Users/lightburnisaac
-echo "gstreamer install done"
-
+if [ -d "/Library/Frameworks/GStreamer.framework" ]; then
+  echo "Cached copy of gstreamer already exists, skipping installation"
+else
+  echo "gstreamer install"
+  echo "Downloading gstreamer runtime package"
+  wget -c --no-check-certificate -nv -Ogstreamer.pkg https://cloud.hm.hozed.net/index.php/s/MmEJPabg9FPnuCL/download
+  echo "Downloading gstreamer devel package"
+  wget -c --no-check-certificate -nv -Ogstreamer-dev.pkg https://cloud.hm.hozed.net/index.php/s/r41LnQOTjf1WG17/download
+  echo "Installing gstreamer runtime package"
+  sudo installer -package gstreamer.pkg -target /;
+  echo "Making a deployment copy of the runtime"
+  sudo cp -R /Library/Frameworks/GStreamer.framework /Library/Frameworks/GStreamer.framework.deploy
+  echo "Installing gstreamer devel package"
+  sudo installer -package gstreamer-dev.pkg -target /;
+  sudo ln -s /Users/travis /Users/lightburnisaac
+  echo "gstreamer install done"
+fi
 
 
