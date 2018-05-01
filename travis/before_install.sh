9279577a7632d0cbfaf05508913c970e8b8f5935
Isaac Lightburn <isaac@openkj.org>
2018-04-30 23:38:17 -0500

    Mac auto builds

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -7,6 +7,7 @@ else
   export BRANCH="unstable"
 fi
 LC_REPO_SLUG=$(echo "$TRAVIS_REPO_SLUG" | tr '[:upper:]' '[:lower:]')
+LC_REPO_SLUG="${LC_REPO_SLUG}-${TRAVIS_BRANCH}"
 export BRANCH_BUCKET=$(echo $LC_REPO_SLUG | sed -e 's/\//-/g') 
 export INSTALLERFN="OpenKJ-${OKJVER}-${BRANCH}-osx-installer.dmg"
 
